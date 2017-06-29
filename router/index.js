a4011d2e295086c7eba9cb96a20d5219914938bb
lianruhe <lianruhe2013@gmail.com>
2017-06-29 09:56:17 +0800

    [M] Add router login

--- router/index.js
+++ router/index.js
@@ -11,6 +11,19 @@ router.get('/api/test', async ctx => {
   }
 })
 
+router.post('/api/signin', async ctx => {
+  const name = ctx.request.body.name || ''
+  const password = ctx.request.body.password || ''
+  console.log(`signin with name: ${name}, password: ${password}`)
+  if (name === 'koa' && password === '123456') {
+    ctx.redirect('/')
+  } else {
+    ctx.response.status = 403
+    ctx.response.body = `<h1>Login failed!</h1>
+    <p><a href="/login.html">Try again</a></p>`
+  }
+})
+
 router.post('/api/upload', async ctx => {
   const file = ctx.request.body.files.file
   const reader = fs.createReadStream(file.path)
